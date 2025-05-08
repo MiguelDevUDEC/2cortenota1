a97275c9773a6664bbcd657039a2cd0ad09f3ecd
MiguelDev <migopa0222@gmail.com>
2025-05-08 15:29:48 -0500

    13

--- 13.cpp
+++ 13.cpp
@@ -1,3 +1,28 @@
 //
 // Created by Miguel on 8/05/2025.
 //
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age;
+    int count = 0;
+
+    cout << "Enter employee ages (negative number to stop): ";
+
+    while (true) {
+        cin >> age;
+        if (age < 0) {
+            break;
+        }
+        if (age > 65) {
+            count++;
+        }
+    }
+
+    cout << "There are " << count << " employees older than 65 years." << endl;
+
+
+    return 0;
+}
